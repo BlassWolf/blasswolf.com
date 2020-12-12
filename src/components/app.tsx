00d593fded83e360350b9aacc082fbe940959cc3
ValeriaVG <valeria.viana.gusmao@gmail.com>
2020-12-12 16:50:38 +0100

    Change name and edit everything

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -7,33 +7,35 @@ import NotFoundPage from "../routes/notfound";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 if ((module as any).hot) {
-  // tslint:disable-next-line:no-var-requires
-  require("preact/debug");
+    // tslint:disable-next-line:no-var-requires
+    require("preact/debug");
 }
 
 const App: FunctionalComponent = () => {
-  let currentUrl: string;
-  const handleRoute = (e: RouterOnChangeArgs) => {
-    currentUrl = e.url;
-  };
+    let currentUrl: string;
+    const handleRoute = (e: RouterOnChangeArgs) => {
+        currentUrl = e.url;
+    };
 
-  return (
-    <div id="app">
-      <Router onChange={handleRoute}>
-        <Route path="/" component={Home} />
+    return (
+        <div id="app">
+            <Router onChange={handleRoute}>
+                <Route path="/" component={Home} />
 
-        <NotFoundPage default />
-      </Router>
-      <footer style={{
-        padding: "1rem",
-        color: "rgba(255,255,255,0.25)",
-        borderTop: "1px solid rgba(255,255,255,0.08)",
-        fontSize: 12,
-      }}>
-        &copy;BlassWolf, 2020
-      </footer>
-    </div>
-  );
+                <NotFoundPage default />
+            </Router>
+            <footer
+                style={{
+                    padding: "1rem",
+                    color: "rgba(255,255,255,0.25)",
+                    borderTop: "1px solid rgba(255,255,255,0.08)",
+                    fontSize: 12
+                }}
+            >
+                &copy;TheWolfSigil, 2020
+            </footer>
+        </div>
+    );
 };
 
 export default App;
